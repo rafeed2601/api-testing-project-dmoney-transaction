1ecb64771819f7700f196f9b6c9fcbef4c797ac4
rafeed2601 <binakbar3456@gmail.com>
2024-01-18 10:47:44 +0600

    Api Testing of Dmoney Transaction

---
+++ report.js
@@ -0,0 +1,18 @@
+const newman = require('newman');
+require('dotenv').config();
+ 
+newman.run({
+    //collection: require('./collection/dmoney.json'),
+    collection:`https://api.postman.com/collections/25942131-1d1c40de-c2c2-4250-9db8-79fddddfed9f?access_key=${process.env.accessKey}`,
+    //environment:require('./collection/env.json'),
+    reporters: 'htmlextra',
+    iterationCount: 1,
+    reporter: {
+        htmlextra: {
+            export: './Reports/report.html', // If not specified, the file will be written to `newman/` in the current working directory.
+        }
+    }
+}, function (err) {
+    if (err) { throw err; }
+    console.log('collection run complete!');
+});
